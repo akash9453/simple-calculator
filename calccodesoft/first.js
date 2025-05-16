948e8ca50e4d12936263c400f5f01058a6fd1a9b
Akash Prajapati <146558237+akash9453@users.noreply.github.com>
2025-05-16 13:32:58 +0530

    Add files via upload

---
+++ calccodesoft/first.js
@@ -0,0 +1,25 @@
+let input= document.querySelector(".inputbox");
+let buttons=document.querySelectorAll("button");
+
+
+let string= " ";
+let arr=Array.from(buttons);
+ arr.forEach(button =>{
+     button.addEventListener("click",(evt)=>{
+          // console.log(evt.target.innerHTML)
+          if(evt.target.innerHTML === "="){
+               string=eval(string);
+               input.value=string;
+          }else if(evt.target.innerHTML==="AC"){
+               string="";
+               input.value=string;
+          }else if(evt.target.innerHTML==="DEL"){
+               string=string.substring(0,string.length-1);
+               input.value=string;
+          }
+          else{
+          string+=evt.target.innerHTML;
+          input.value=string;
+          }
+     })
+})   
